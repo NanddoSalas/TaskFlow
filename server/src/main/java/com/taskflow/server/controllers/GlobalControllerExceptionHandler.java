c657b1d867b8dd3bc15782a5e9c8858a162babfa
Luis Fernando Cano Salas <luis.nando.salas28@gmail.com>
2025-03-18 14:45:38 -0600

    handle MethodArgumentNotValidException

---
+++ server/src/main/java/com/taskflow/server/controllers/GlobalControllerExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.taskflow.server.controllers;
+
+import com.taskflow.server.dtos.ResponseDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalControllerExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDTO> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return ResponseDTO.badRequest(errors);
+    }
+}
