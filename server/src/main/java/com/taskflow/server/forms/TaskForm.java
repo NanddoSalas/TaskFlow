a1088c79db0a1eb3038e2ffa34b7ad765dea9f35
Luis Fernando Cano Salas <luis.nando.salas28@gmail.com>
2025-03-18 14:44:19 -0600

    update TaskForm and remove EnumValidator

--- server/src/main/java/com/taskflow/server/forms/TaskForm.java
+++ server/src/main/java/com/taskflow/server/forms/TaskForm.java
@@ -1,5 +1,6 @@
 package com.taskflow.server.forms;
 
+import com.taskflow.server.entities.Task;
 import com.taskflow.server.enums.TaskPriority;
 import com.taskflow.server.enums.TaskStatus;
 import com.taskflow.server.validators.ValidEnum;
@@ -20,11 +21,22 @@ public class TaskForm {
     private String description;
 
     @NotNull
-    @ValidEnum(enumClass = TaskPriority.class)
+//    @ValidEnum(enumClass = TaskPriority.class)
     private TaskPriority priority;
 
     @NotNull
-    @ValidEnum(enumClass = TaskStatus.class)
+//    @ValidEnum(enumClass = TaskStatus.class)
     private TaskStatus status;
+
+    public Task getTask() {
+        Task task = new Task();
+
+        task.setTitle(title);
+        task.setDescription(description);
+        task.setPriority(priority);
+        task.setStatus(status);
+
+        return task;
+    }
 
 }
