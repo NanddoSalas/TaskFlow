420df2b6abf2523b6baf8d67d85c0ed921a71653
Luis Fernando Cano Salas <luis.nando.salas28@gmail.com>
2025-03-18 14:45:03 -0600

    update Board and Task repositories

--- server/src/main/java/com/taskflow/server/repositories/BoardRepository.java
+++ server/src/main/java/com/taskflow/server/repositories/BoardRepository.java
@@ -1,7 +1,16 @@
 package com.taskflow.server.repositories;
 
 import com.taskflow.server.entities.Board;
+import com.taskflow.server.entities.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface BoardRepository extends CrudRepository<Board, Integer> {
+
+    public List<Board> findAllByOwnerId(int ownerId);
+    public Optional<Board> findByIdAndOwnerId(int id, int ownerId);
+    public void deleteBoardByIdAndOwnerId(int boardId, int ownerId);
+
 }
--- server/src/main/java/com/taskflow/server/repositories/TaskRepository.java
+++ server/src/main/java/com/taskflow/server/repositories/TaskRepository.java
@@ -3,5 +3,13 @@ package com.taskflow.server.repositories;
 import com.taskflow.server.entities.Task;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface TaskRepository extends CrudRepository<Task, Integer> {
+
+    public List<Task> findAllByBoardId(int boardId);
+    public Optional<Task> findTaskByIdAndBoardId(int taskId, int boardId);
+    public void deleteByIdAndBoardId(int taskId, int boardId);
+
 }
