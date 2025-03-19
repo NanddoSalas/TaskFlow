1ec27587361f212fd39af04cad37daf11ea8b42e
Luis Fernando Cano Salas <luis.nando.salas28@gmail.com>
2025-03-19 10:03:47 -0600

    add task controller and service

--- server/src/main/java/com/taskflow/server/controllers/TaskController.java
+++ server/src/main/java/com/taskflow/server/controllers/TaskController.java
@@ -2,7 +2,8 @@ package com.taskflow.server.controllers;
 
 import com.taskflow.server.dtos.ResponseDTO;
 import com.taskflow.server.entities.User;
-import com.taskflow.server.forms.TaskForm;
+import com.taskflow.server.forms.CreateTaskForm;
+import com.taskflow.server.forms.PatchTaskForm;
 import com.taskflow.server.services.TaskService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -18,26 +19,26 @@ public class TaskController {
     private final TaskService taskService;
 
     @GetMapping("/tasks")
-    public ResponseEntity<ResponseDTO> getTasks(@AuthenticationPrincipal User user, @PathVariable int boardId) {
-        return ResponseDTO.ok(taskService.getAllTasks(boardId, user.getId()));
+    public ResponseEntity<ResponseDTO> retrieveTasks(@AuthenticationPrincipal User user, @PathVariable int boardId) throws Exception {
+        return ResponseDTO.ok(taskService.retrieveTasks(user.getId(), boardId));
     }
 
-    @PostMapping("/tasks")
+    @PostMapping("/groups/{groupId}/task")
     public ResponseEntity<ResponseDTO> createTask(@AuthenticationPrincipal User user, @PathVariable int boardId,
-            @Valid @RequestBody TaskForm taskForm) {
-        return ResponseDTO.ok(taskService.createTask(boardId, taskForm, user.getId()));
+                                                  @Valid @RequestBody CreateTaskForm form, @PathVariable int groupId) throws Exception {
+        return ResponseDTO.ok(taskService.createTask(user.getId(), boardId, groupId, form));
     }
 
-    @PutMapping("/tasks/{taskId}")
+    @PutMapping("/groups/{groupId}/tasks/{taskId}")
     public ResponseEntity<ResponseDTO> updateTask(@AuthenticationPrincipal User user, @PathVariable int taskId,
-            @Valid @RequestBody TaskForm taskForm, @PathVariable int boardId) {
-        return ResponseDTO.ok(taskService.updateTask(boardId, taskId, taskForm, user.getId()));
+                                                  @Valid @RequestBody PatchTaskForm form, @PathVariable int boardId, @PathVariable int groupId) throws Exception {
+        return ResponseDTO.ok(taskService.updateTask(user.getId(), boardId, groupId, taskId, form));
     }
 
-    @DeleteMapping("/tasks/{taskId}")
+    @DeleteMapping("/groups/{groupId}/tasks/{taskId}")
     public ResponseEntity<ResponseDTO> deleteTask(@AuthenticationPrincipal User user, @PathVariable int taskId,
-            @PathVariable int boardId) {
-        taskService.deleteTask(boardId, taskId, user.getId());
+                                                  @PathVariable int boardId, @PathVariable int groupId) throws Exception {
+        taskService.deleteTask(user.getId(), boardId, groupId, taskId);
         return ResponseDTO.ok(null);
     }
 }
--- server/src/main/java/com/taskflow/server/services/TaskService.java
+++ server/src/main/java/com/taskflow/server/services/TaskService.java
@@ -1,10 +1,11 @@
 package com.taskflow.server.services;
 
 import com.taskflow.server.dtos.TaskDTO;
-import com.taskflow.server.entities.Board;
 import com.taskflow.server.entities.Task;
-import com.taskflow.server.forms.TaskForm;
+import com.taskflow.server.forms.CreateTaskForm;
+import com.taskflow.server.forms.PatchTaskForm;
 import com.taskflow.server.repositories.BoardRepository;
+import com.taskflow.server.repositories.GroupRepository;
 import com.taskflow.server.repositories.TaskRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
@@ -19,68 +20,66 @@ public class TaskService {
 
     private final TaskRepository taskRepository;
     private final BoardRepository boardRepository;
+    private final GroupRepository groupRepository;
 
-    public List<TaskDTO> getAllTasks(int boardId, int userId) {
-        Optional<Board> board = boardRepository.findByIdAndOwnerId(boardId, userId);
+    public List<TaskDTO> retrieveTasks(int userId, int boardId) throws Exception {
+        boardRepository.findByIdAndOwnerId(boardId, userId).orElseThrow(() -> new Exception("Board doesn't exist"));
 
-        if (board.isPresent()) {
-            return taskRepository.findAllByBoardId(boardId).stream().map(Task::toDTO).toList();
-        } else {
-            // exception: board doesn't exist or user is not the owner of the board
-            System.out.println("exception: board doesn't exist or user is not the owner of the board");
-            return null;
-        }
+        return taskRepository.findAllByBoardId(boardId).stream().map(Task::toDTO).toList();
     }
 
-    public TaskDTO createTask(int boardId, TaskForm form, int userId) {
-        Optional<Board> board = boardRepository.findByIdAndOwnerId(boardId, userId);
+    public TaskDTO createTask(int userId, int boardId, int groupId, CreateTaskForm form) throws Exception {
+        boardRepository.findByIdAndOwnerId(boardId, userId).orElseThrow(() -> new Exception("Board doesn't exist"));
+        groupRepository.findByIdAndBoardId(groupId, boardId).orElseThrow(() -> new Exception("Group doesn't exist"));
 
-        if (board.isPresent()) {
-            Task task = form.getTask();
-            task.setBoard(board.get());
+        Task task = new Task();
 
-            return taskRepository.save(task).toDTO();
+        task.setTitle(form.getTitle());
+        task.setDescription(form.getDescription());
+        task.setBoardId(boardId);
+        task.setGroupId(groupId);
+
+        Optional<Task> taskOptional = taskRepository.findFirstByOrderByPositionDesc();
+
+        if (taskOptional.isPresent()) {
+            long newPosition = taskOptional.get().getPosition() + 10000;
+            task.setPosition(newPosition);
         } else {
-            // exception: board doesn't exist or user is not the owner of the board
-            System.out.println("exception: board doesn't exist or user is not the owner of the board");
-            return null;
+            task.setPosition(0);
         }
+
+        return taskRepository.save(task).toDTO();
     }
 
-    public TaskDTO updateTask(int boardId, int taskId, TaskForm form, int userId) {
-        Optional<Board> board = boardRepository.findByIdAndOwnerId(boardId, userId);
+    public TaskDTO updateTask(int userId, int boardId, int groupId, int taskId, PatchTaskForm form) throws Exception {
+        boardRepository.findByIdAndOwnerId(boardId, userId).orElseThrow(() -> new Exception("Board doesn't exist"));
+        groupRepository.findByIdAndBoardId(groupId, boardId).orElseThrow(() -> new Exception("Group doesn't exist"));
+        Task task = taskRepository.findByIdAndBoardIdAndGroupId(taskId, boardId, groupId).orElseThrow(() -> new Exception("Task doesn't exist"));
 
-        if (board.isPresent()) {
-            Optional<Task> taskOptional = taskRepository.findTaskByIdAndBoardId(taskId, boardId);
-
-            if (taskOptional.isPresent()) {
-                Task task = taskOptional.get();
-
-                task.setTitle(form.getTitle());
-                task.setDescription(form.getDescription());
-                task.setPriority(form.getPriority());
-                task.setStatus(form.getStatus());
-
-                return taskRepository.save(task).toDTO();
-            } else {
-                // exception: task belongs to a different board
-                System.out.println("exception: task belongs to a different board");
-                return null;
-            }
-
-        } else {
-            // exception: board doesn't exist or user is not the owner of the board
-            System.out.println("exception: board doesn't exist or user is not the owner of the board");
-            return null;
+        if (form.getTitle() != null) {
+            task.setTitle(form.getTitle());
         }
+
+        if (form.getDescription() != null) {
+            task.setDescription(form.getDescription());
+        }
+
+        if (form.getPosition() != null) {
+            task.setPosition(form.getPosition());
+        }
+
+        if (form.getGroupId() != null) {
+            groupRepository.findByIdAndBoardId(form.getGroupId(), boardId).orElseThrow(() -> new Exception("Group doesn't exist"));
+            task.setGroupId(form.getGroupId());
+        }
+
+        return taskRepository.save(task).toDTO();
     }
 
     @Transactional
-    public void deleteTask(int boardId, int taskId, int userId) {
-        Optional<Board> board = boardRepository.findByIdAndOwnerId(boardId, userId);
+    public void deleteTask(int userId, int boardId, int groupId, int taskId) throws Exception {
+        boardRepository.findByIdAndOwnerId(boardId, userId).orElseThrow(() -> new Exception("Board doesn't exist"));
 
-        if (board.isPresent()) {
-            taskRepository.deleteByIdAndBoardId(taskId, boardId);
-        }
+        taskRepository.deleteByIdAndBoardIdAndGroupId(taskId, boardId, groupId);
     }
 }
