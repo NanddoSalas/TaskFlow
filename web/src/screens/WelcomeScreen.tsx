2c78d0a4d3b8c33a9e9941f50a2dcf5ddba4edbb
Luis Fernando Cano Salas <luis.nando.salas28@gmail.com>
2025-03-22 21:32:51 -0600

    implement Google authentication logic
      * GoogleOAuthProvider is yet to be implemented

--- web/src/screens/WelcomeScreen.tsx
+++ web/src/screens/WelcomeScreen.tsx
@@ -1,3 +1,4 @@
+import { CredentialResponse, GoogleLogin } from '@react-oauth/google';
 import {
   Grab,
   KeyRound,
@@ -6,8 +7,7 @@ import {
   StretchVertical,
 } from 'lucide-react';
 import { useBearStore } from '../bearState';
-import { Button } from '../components/ui/button';
-import { fakeData } from '../fakeData';
+import { getUserFromIdToken } from '../utils';
 
 const features = [
   {
@@ -40,6 +40,14 @@ const features = [
 
 export const WelcomeScreen = () => {
   const login = useBearStore((state) => state.login);
+
+  const handleLogin = ({ credential }: CredentialResponse) => {
+    if (credential) {
+      console.log(credential);
+      const user = getUserFromIdToken(credential);
+      login(credential, user);
+    }
+  };
 
   return (
     <div className="overflow-hidden bg-white py-8 sm:py-16 md:py-24 lg:py-32">
@@ -77,19 +85,12 @@ export const WelcomeScreen = () => {
               </dl>
 
               <div className="mt-8">
-                <Button
-                  variant="outline"
-                  className="w-full"
-                  onClick={() => login(fakeData.idToken!, fakeData.user!)}
-                >
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <path
-                      d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                      fill="currentColor"
-                    />
-                  </svg>
-                  Continue with Google
-                </Button>
+                <GoogleLogin
+                  onSuccess={handleLogin}
+                  onError={() => {
+                    console.log('Login Failed');
+                  }}
+                />
               </div>
             </div>
           </div>
--- web/src/utils.ts
+++ web/src/utils.ts
@@ -1,3 +1,17 @@
+import * as jose from 'jose';
+import { User } from './types';
+
 export function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ');
 }
+
+export const getUserFromIdToken = (idToken: string) => {
+  const claims = jose.decodeJwt(idToken);
+  const user: User = { name: '', email: '', picture: '' };
+
+  if ('name' in claims) user.name = claims['name'] as string;
+  if ('email' in claims) user.email = claims['email'] as string;
+  if ('picture' in claims) user.picture = claims['picture'] as string;
+
+  return user;
+};
