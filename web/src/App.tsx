85d96494853e5af45635cb774f8cb6f42d2c8214
Luis Fernando Cano Salas <luis.nando.salas28@gmail.com>
2025-03-23 22:03:24 -0600

    setup simple zustand devtools in development mode

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,12 @@
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { mountStoreDevtool } from 'simple-zustand-devtools';
+import { useBearStore } from './bearState';
 import { Auth } from './components/Auth';
 import './index.css';
+
+if (process.env.NODE_ENV === 'development') {
+  mountStoreDevtool('Store', useBearStore);
+}
 
 export default function App() {
   return (
